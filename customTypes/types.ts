a75d6b0ea6f9b751b95474aab3839552d90e501e
Nauman <nauman001@yahoo.com>
2024-07-02 23:43:26 +0200

    added custom types

---
+++ customTypes/types.ts
@@ -0,0 +1,71 @@
+export interface SpotlightAnimes {
+    rank: number;
+    id: string;
+    name: string;
+    description: string;
+    poster: string;
+    jname: string;
+    episodes: {
+        sub: number;
+        dub: number;
+    };
+    otherInfo: string[];
+}
+
+export interface TrendingAnimes {
+    rank: number;
+    name: string;
+    id: string;
+    poster: string;
+}
+
+export interface HomeAnimes {
+    spotlightAnimes: SpotlightAnimes[];
+    trendingAnimes: TrendingAnimes[];
+}
+
+export interface SliderProps {
+    spotlightAnimes: SpotlightAnimes[];
+}
+
+export interface AnimeCarouselProps {
+    trendingAnimes: TrendingAnimes[];
+}
+
+// anime info
+export interface Info {
+    id: string;
+    name: string;
+    description: string;
+    poster: string;
+    stats: {
+        rating: string;
+        quality: string;
+        episodes: {
+            sub: number;
+            dub: number;
+        };
+        type: string;
+        duration: string;
+    };
+}
+
+// anime extra info
+export interface MoreInfo {
+    japanese: string;
+    aired: string;
+    premiered: string;
+    duration: string;
+    status: string;
+    malscore: string;
+    genres: string[];
+    studios: string;
+    producers: string[];
+}
+
+export interface AnimeData {
+    anime: {
+        info: Info;
+        moreInfo: MoreInfo;
+    };
+}
