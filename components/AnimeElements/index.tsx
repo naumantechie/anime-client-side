e8dd23c3a47e4766603a24023985916494c82dd1
Nauman <nauman001@yahoo.com>
2024-07-13 18:41:37 +0200

    added badge component

--- components/AnimeElements/index.tsx
+++ components/AnimeElements/index.tsx
@@ -1,36 +1,61 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+import { CSSProperties } from 'react';
+
+interface BadgeProps {
+    text?: string | number;
+    iconType?: IconDefinition;
+    hasborderRadius?: boolean;
+    textColor?: string;
+    bgColor?: string;
+}
 
 export default function Badge({
     text,
     iconType,
-    hasborderRadius,
-    textColor,
-    bgColor,
-}) {
+    hasborderRadius = false,
+    textColor = '#111111',
+    bgColor = 'transparent',
+}: BadgeProps) {
+    if (!text) return;
+
     switch (bgColor) {
         case 'pink':
             bgColor = '#ffbade';
+            break;
         case 'green':
             bgColor = '#b0e3af';
+            break;
         case 'blue':
             bgColor = '#b9e7ff';
+            break;
         case 'gray':
             bgColor = '#3a3951';
-        default:
-            bgColor = 'tranperant';
+            break;
     }
 
-    const styles = {
+    const styles: CSSProperties = {
         backgroundColor: bgColor,
-        color: textColor || '#111111',
-        padding: '1px 4px',
+        color: textColor,
+        padding: '2px 4px',
         fontSize: '12px',
         lineHeight: '1.2em',
         fontWeight: '700',
         borderRadius: hasborderRadius ? '3px' : '0',
+        display: 'inline-flex',
+        alignItems: 'center',
+        height: '100%',
     };
 
-    const icon = iconType ? <FontAwesomeIcon icon={iconType} /> : null;
+    const icon = iconType ? (
+        <FontAwesomeIcon
+            style={{ marginRight: '5px' }}
+            width={'auto'}
+            height={13}
+            icon={iconType}
+        />
+    ) : null;
 
     return (
         <span style={styles}>
